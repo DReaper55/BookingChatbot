eb2cfb7c14170434ca858910196e869e95154974
Daniel Uwadi <danieluwadi@yahoo.com>
2025-03-10 18:54:40 +0100

    Changed routing and requirements

--- src/api/app.py
+++ src/api/app.py
@@ -35,21 +35,23 @@ app.add_middleware(
 # Include routers for HTTP and WebSocket endpoints
 app.include_router(http_router)
 app.include_router(auth_router, prefix="/auth")
-app.include_router(ws_router)
+# app.include_router(ws_router)
 
-def start_server():
-    # Start Docker container (if not already running)
-    # try:
-    #     subprocess.run(["docker-compose", "up", "-d"], check=True)
-    #     print("Docker container started successfully.")
-    # except subprocess.CalledProcessError:
-    #     print("Failed to start Docker container. Ensure it exists.")
+# ProductsRetrievalService().sync_mongo_to_opensearch()
 
-    # Sync databases
-    ProductsRetrievalService().sync_mongo_to_opensearch()
-
-    # Start server
-    subprocess.run(["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000", "--reload"])
-
-if __name__ == "__main__":
-    start_server()
+# def start_server():
+#     # Start Docker container (if not already running)
+#     # try:
+#     #     subprocess.run(["docker-compose", "up", "-d"], check=True)
+#     #     print("Docker container started successfully.")
+#     # except subprocess.CalledProcessError:
+#     #     print("Failed to start Docker container. Ensure it exists.")
+#
+#     # Sync databases
+#     ProductsRetrievalService().sync_mongo_to_opensearch()
+#
+#     # Start server
+#     subprocess.run(["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000", "--reload"])
+#
+# if __name__ == "__main__":
+#     start_server()
